ff886086181d2e3c328ef0d0192bf37ae5665a1b
mahbub <mahbub.shuvo10@gmail.com>
2024-10-01 23:01:24 +0200

    Added Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+	{
+	path: '',
+	redirectTo: 'login',
+	pathMatch: 'full'
+	},
+
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+
+	{
+		path: '',
+		component:LayoutComponent,
+		children:[
+			{
+				path: 'dashboard'
+				component: DashboardComponent
+			},
+			{
+				path: 'employee'
+				component: EmployeeComponent
+			},
+			{
+				path: 'projects'
+				component: EmployeeComponent
+			},
+			{
+				path: 'dashboard'
+				component: EmployeeComponent
+			}
+		]
+	}
+];
+	

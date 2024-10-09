58537bf3966d9bc2194e1cb99891b0454f8efb49
mahbub <mahbub.shuvo10@gmail.com>
2024-10-09 19:52:29 +0200

    Fixed FormsModule

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',

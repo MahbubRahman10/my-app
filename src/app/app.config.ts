b3a3967a32c6a15c6d84b4ac5e8afbdc2a536a1a
mahbub <mahbub.shuvo10@gmail.com>
2024-10-22 22:35:59 +0200

    Added Department API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,5 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient, provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
 };
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MasterService } from '../service/master.service';
+import { IApiResponse, IParentDept } from '../model/interface/master';
 
 @Component({
   selector: 'app-employee',
@@ -7,8 +9,21 @@ import { Component, signal } from '@angular/core';
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
 
   isFormVisiable = signal<boolean>(false);
 
+  masterSrv = inject(MasterService);
+  
+  parentDeptList = signal<IParentDept[]>([])
+
+  ngOnInit(): void {
+    this.getParentDept();
+  }
+
+  getParentDept(){
+    this.masterSrv.getAlldept().subscribe((res:IApiResponse) =>{
+      this.parentDeptList.set(res.data);
+    })
+  }
 }
